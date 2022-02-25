d6b0a01a125117cec6f0512374265e2035dba503
Mau Abata <mauabata@gmail.com>
2022-02-25 16:28:54 -0600

    Add scrollTo to BOMList. Will make that a generic container for StationLIst later.

--- src/components/BOMList.tsx
+++ src/components/BOMList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { IPOSData } from './POSFileLoader';
 import './BOMList.scss';
 
@@ -11,15 +11,32 @@ interface IBOMListProps {
 const BOMList: React.FC<IBOMListProps> = ({
   posData, selectedPart, onPartSelect,
 }) => {
+  const ulRef = useRef<HTMLUListElement>(null);
+  const selectedPartRef = useRef<HTMLLIElement>(null);
+
   const handlePartClick = (reference: string) => (e: React.MouseEvent) => {
     e.preventDefault();
     onPartSelect(reference);
   };
 
+  useEffect(() => {
+    if (ulRef.current) {
+      let top = selectedPartRef.current ? selectedPartRef.current.offsetTop : null;
+      if (top !== null) {
+        top -= ulRef.current.clientHeight / 2;
+        ulRef.current.scrollTo({ top, behavior: 'smooth' });
+      }
+    }
+  }, [selectedPart, selectedPartRef]);
+
   return (
-    <ul className="bom-list">
+    <ul className="bom-list" ref={ulRef}>
       {posData.map((part) => (
-        <li key={part.reference} className={part.reference === selectedPart ? ' active' : ''}>
+        <li
+          key={part.reference}
+          className={part.reference === selectedPart ? ' active' : ''}
+          ref={part.reference === selectedPart ? selectedPartRef : null}
+        >
           <button type="button" onClick={handlePartClick(part.reference)}>
             <div className="reference">{part.reference}</div>
             <div className="details">
